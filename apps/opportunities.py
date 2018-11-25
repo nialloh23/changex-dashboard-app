65e4f94bb01355f676e6129826783128e58a6768
Niall O'Hara <niall@changex.org>
2018-11-25 13:14:33 +0000

    removed dash table module

--- apps/opportunities.py
+++ apps/opportunities.py
@@ -14,7 +14,7 @@ import dash
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_table
+#import dash_table
 
 import plotly.plotly as py
 from plotly import graph_objs as go
